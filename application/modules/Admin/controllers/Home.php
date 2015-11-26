1abd69db75275e44508562f03746763bf3740db3
Administrator <511202838@qq.com>
2015-11-26 11:36:39 +0800

    修改显示网站title和说明

--- application/modules/Admin/controllers/Home.php
+++ application/modules/Admin/controllers/Home.php
@@ -17,8 +17,11 @@ class HomeController extends AdminBase {
      * 对于如下的例子, 当访问http://yourhost/Sample/home/home/home/name/desktop-uabd7nl\administrator 的时候, 你就会发现不同
      */
 	public function indexAction($name = "Stranger") {
+		$this->getView()->assign("title", "H+ 后台主题UI框架 - 主页");
+		$this->getView()->assign("keywords", "H+后台主题,后台bootstrap框架,会员中心主题,后台HTML,响应式后台");
+		$this->getView()->assign("description", "H+是一个完全响应式，基于Bootstrap3最新版本开发的扁平化主题，她采用了主流的左右两栏式布局，使用了Html5+CSS3等现代技术");
 		//1. fetch query
-		$get = $this->getRequest()->getQuery("get", "default value");
+		//$get = $this->getRequest()->getQuery("get", "default value");
 
 		//2. fetch model
 		$model = new SampleModel();
@@ -26,9 +29,7 @@ class HomeController extends AdminBase {
 		//3. assign
 		$this->getView()->assign("content", "Admin登录");
 		$this->getView()->assign("name", "AdminName登录");
-		//$this->getView()->assign("title", "H+ 后台主题UI框架 - 主页");
-		//$this->getView()->assign("keywords", "H+后台主题,后台bootstrap框架,会员中心主题,后台HTML,响应式后台");
-		$this->getView()->assign("description", "H+是一个完全响应式，基于Bootstrap3最新版本开发的扁平化主题，她采用了主流的左右两栏式布局，使用了Html5+CSS3等现代技术");
+
 		//在控制器里手动调用的方式有2种
 		//$this->display('hello');
 		//$this->getView()->display('test/world.phtml');
